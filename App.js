be30668b4a0a73b561ef27184bf9188f0ab8bd54
koussaila <kadi.koussaila@outlook.fr>
2024-04-25 16:08:15 +0200

    commit 0.0.1

--- App.js
+++ App.js
@@ -1,12 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+const Home =()=>{
+  return(
+    <View style={styles.container}>
+      <Text>Home!</Text>
+    </View>
+  )
+};
+const Profile =()=>{
+  return(
+    <View style={styles.container}>
+      <Text>Profile!</Text>
+    </View>
+  )
+};
+const Photos = ()=>{
+  return(
+    <View style={styles.container}>
+      <Text>Photos!</Text>
+    </View>
+  )
+};
+const MyDrawer = ()=> {
+  return(
+      <Drawer.Navigator 
+        screenOptions={
+          {drawerStyle:{width:250},
+          }
+        }
+        >
+        <Drawer.Screen name="Profile" component= {Profile}/>
+        <Drawer.Screen name="Photos" component={Photos}/>
+      </Drawer.Navigator>
+  )
+}; 
+const MyTabs =()=>{
+  return(
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: 'lightblue' },
+          headerTintColor: 'white',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      > 
+        <Tab.Screen name="Home" component={Home}
+        />
+        <Tab.Screen name="Drawer" component={MyDrawer} options={{headerShown: true, lazy:true}}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+};
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <MyTabs/>
   );
 }
 
